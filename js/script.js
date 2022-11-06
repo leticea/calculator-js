bdd375c1a49e75ff73f8f9d0eab09c779fa92b4f
leticea <nascimento.leticeah@gmail.com>
2022-11-06 11:27:13 -0300

    selecionando elementos no js

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const previousOperationText = document.querySelector("#previous-operation");
+const currentOperationText = document.querySelector("#current-operation");
+const buttons = document.querySelectorAll("#buttons-container button");
+
+class Calculator {
+    
+}
