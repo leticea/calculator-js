fd90beba56242b8bb290e3abd7db89fcbfdc875f
leticea <nascimento.leticeah@gmail.com>
2022-11-06 11:38:39 -0300

    criando função de click nos botões

--- js/script.js
+++ js/script.js
@@ -11,7 +11,15 @@ buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
 
         const value = e.target.innerText;
+        
+        if (value >= 0 || value === ".") {
 
-        console.log(value);
-    })
+            console.log(value);
+
+        } else {
+
+            console.log("Op: " + value);
+        }
+                
+    });
 });
