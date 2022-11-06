9fb5a0288f120e8f4bfdabad547f9e1f50c9de74
leticea <nascimento.leticeah@gmail.com>
2022-11-06 11:32:29 -0300

    criando função de click nos botões

--- js/script.js
+++ js/script.js
@@ -3,5 +3,15 @@ const currentOperationText = document.querySelector("#current-operation");
 const buttons = document.querySelectorAll("#buttons-container button");
 
 class Calculator {
-    
+
 }
+
+buttons.forEach((btn) => {
+
+    btn.addEventListener("click", (e) => {
+
+        const value = e.target.innerText;
+
+        console.log(value);
+    })
+});
